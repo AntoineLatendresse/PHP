274b0330e0dc0ea989d694c2f950fcc34d482228
AntoineLatendresse <a.latendresse2@hotmail.com>
2015-10-30 12:22:51 -0400

    Presque fini

--- Labo1_PHP/ScriptController.php
+++ Labo1_PHP/ScriptController.php
@@ -13,14 +13,47 @@ if(isset($_POST["Animaux"]))
 
 function Vote($Vote)
 {
-    //Create File Name
+    $chien = 0;
+    $chat = 0;
+    $oiseau = 0;
+    $serpent = 0;
+    $singe = 0;
+
+    //premiere approche
+
+    /*//Create File Name
     $myfile = fopen("FichierText.txt", "w");
 
     //Enter values from website to variables
     $radioButtonSelected = $_POST['Animaux'];
 
     //Write values in file
-    fwrite($myfile, $radioButtonSelected . "\n");
+    fwrite($myfile, $radioButtonSelected . "\n");*/
+
+
+    //Deuxieme approche
+    switch($_POST['Animaux']) {
+        case "Chien":
+            $chien++;
+            echo $chien;
+            break;
+        case "Chat":
+            $chat++;
+            echo $chat;
+            break;
+        case "Oiseau":
+            $oiseau++;
+            echo $oiseau;
+            break;
+        case "Serpent":
+            $serpent++;
+            echo $serpent;
+            break;
+        case "Singe":
+            $singe++;
+            echo $singe;
+            break;
+    }
 }
 
 ?>
