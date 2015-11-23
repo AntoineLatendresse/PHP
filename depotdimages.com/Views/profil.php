80ffea357ea4127d3919448ed8c7d6e1c78b9baf
AntoineLatendresse <a.latendresse2@hotmail.com>
2015-11-23 16:11:32 -0500

    Page Profil Ajouter avec rien de dans lol

--- depotdimages.com/Views/profil.php
+++ depotdimages.com/Views/profil.php
@@ -8,7 +8,33 @@ showHeader();
  */
 
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <link href="../Styles/style.css" rel="stylesheet" media="all" type="text/css">
+</head>
+<body>
+    <div class="wrap">
 
+        <form action="<?php echo CONTROLLER_UPDATE_PASSWORD ?>" method="post">
+
+            <label>Nom d'usager :</label>
+
+            <label>Mot de passe :</label>
+
+            <label>Prenom :</label>
+
+            <label>Nom :</label>
+
+            <a href="">
+                <input  type="submit" value="Modifier..." class="button" />
+            </a>
+
+        </form>
+</div>
+</body>
+</html>
 <?php
 showFooter();
 ?>
