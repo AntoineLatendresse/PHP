aaf17bd8891dc28277fb78fec1293943ef64ac1b
AntoineLatendresse <a.latendresse2@hotmail.com>
2015-12-04 14:46:59 -0500

    Uplaod de base

--- depotdimages.com/Controller/functions.php
+++ depotdimages.com/Controller/functions.php
@@ -347,55 +347,24 @@ function getGestImages($image)
 //****UploadImage******************************************************************************************************/
 function upload_Image()
 {
-   /* $target_dir = '../images';
-    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-    $uploadOk = 1;
-    $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-    // Check if image file is a actual image or fake image
-    if(isset($_POST["filename"])) {
-        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-        if($check !== false) {
-            echo "File is an image - " . $check["mime"] . ".";
-            $uploadOk = 1;
-        } else {
-            echo "File is not an image.";
-            $uploadOk = 0;
-        }
-    }
-    // Check if file already exists
-    if (file_exists($target_file)) {
-        echo "Sorry, file already exists.";
-        $uploadOk = 0;
-    }
-// Check file size
-    if ($_FILES["fileToUpload"]["size"] > 2097152) {
-        echo "Sorry, your file is too large.";
-        $uploadOk = 0;
-    }
-// Allow certain file formats
-    if($imageFileType != "gif" && $imageFileType != "jpg" && $imageFileType != "jpe" && $imageFileType != "jpeg")
-        && $imageFileType != "gif" ) {
-        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-        $uploadOk = 0;
-    }
-// Check if $uploadOk is set to 0 by an error
-    if ($uploadOk == 0) {
-        echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-    } else {
-        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-            echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-        } else {
-            echo "Sorry, there was an error uploading your file.";
-        }
-    }
-    $ext_type = array('gif','jpg','jpe','jpeg','png');
-    $maxsize    = 2097152;
-
     if(isset($_POST['upload_img']))
     {
-        $check
-    }*/
+        $ext_type = array('gif','jpg','jpe','jpeg','png');
+        $maxsize    = 2097152;
+
+        if(isset($_POST['upload_img']))
+        {
+            $file_name = $_FILES['image']['name'];
+            $file_type = $_FILES['image']['type'];
+            $file_size = $_FILES['image']['size'];
+            $file_tmp_name = $_FILES['image']['tmp_name'];
+
+            if(move_uploaded_file($file_tmp_name, "../images/$file_name"))
+            {
+                header("Refresh:0");
+            }
+        }
+    }
 }
 //****UploadImage******************************************************************************************************/
 
